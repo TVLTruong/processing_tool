04f95dd3d7ed313e0d66e2296e95a1279d57d48c
LamTruong <tvlamtruong@gmail.com>
2025-03-23 21:01:41 +0700

    Fix deployment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FileUploader from "./FileUploader";
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="/pressing_tool">
+    <Router>
       <div className="App">
         <h1>Upload và Đọc File CSV</h1>
         <Routes>
           <Route path="/" element={<FileUploader />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
