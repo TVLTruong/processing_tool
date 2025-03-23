e658921e022c8141d4765ceee9dcbb4ae2c2d51b
LamTruong <tvlamtruong@gmail.com>
2025-03-23 20:45:26 +0700

    Deploy React App to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import FileUploader from './FileUploader';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FileUploader from "./FileUploader";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Upload và Đọc File CSV</h1>
-      <FileUploader />
-    </div>
+    <BrowserRouter basename="/pressing_tool">
+      <div className="App">
+        <h1>Upload và Đọc File CSV</h1>
+        <Routes>
+          <Route path="/" element={<FileUploader />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Thêm dòng này
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // Không bọc thêm BrowserRouter ở đây
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/pressing_tool"> {/* Bọc App trong BrowserRouter */}
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
