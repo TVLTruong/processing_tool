081e7aa08d4ab0a3c7e49a1e8dba009dbb056ec8
LamTruong <tvlamtruong@gmail.com>
2025-03-23 21:03:17 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FileUploader from "./FileUploader";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
